5541d8c4f413f122dac93bceb28c15fdba657101
yazan <yazan.mahfood.419@gmail.com>
2024-05-12 12:31:03 +0300

    Delete Recommendation

--- src/main/java/com/musikrec/musikrec/Services/RecommendationService.java
+++
@@ -1,8 +0,0 @@
-package com.musikrec.musikrec.Services;
-
-import com.musikrec.musikrec.Models.Recommendation;
-
-public interface RecommendationService {
-
-    Recommendation getRecommendationById (Long id);
-}
