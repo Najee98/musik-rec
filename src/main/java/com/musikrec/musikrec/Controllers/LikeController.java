c36be7faae4de56fce13ef493f9b31be1ef01cbe
yazan <yazan.mahfood.419@gmail.com>
2024-05-23 21:29:26 +0300

    Add Message Response To APIs :addLike &deleteLike

--- src/main/java/com/musikrec/musikrec/Controllers/LikeController.java
+++ src/main/java/com/musikrec/musikrec/Controllers/LikeController.java
@@ -1,9 +1,9 @@
 package com.musikrec.musikrec.Controllers;
 
 import com.musikrec.musikrec.Dto.Responses.LikeResponseDto;
-import com.musikrec.musikrec.Models.Like;
 import com.musikrec.musikrec.Services.LikeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,13 +26,21 @@ public class LikeController {
 
 
     @PostMapping("/add")
-    public void insertLike(@RequestParam Long songId, @RequestParam Long userId) {
+    public ResponseEntity<Object> insertLike(
+            @RequestParam Long songId,
+            @RequestParam Long userId){
         likeService.insertLike(songId, userId);
+
+        return new ResponseEntity<>("{ \"message\": \" Added like to song successfully  \" }" , HttpStatus.OK);
+
     }
 
 
     @DeleteMapping("/delete")
-    public void deleteLike(@RequestParam Long id) {
+    public ResponseEntity<Object> deleteLike(@RequestParam Long id) {
         likeService.deleteLike(id);
+
+        return new ResponseEntity<>("{\"message\": \" Deleted like from the song successfully  \" }", HttpStatus.OK);
+
     }
 }
